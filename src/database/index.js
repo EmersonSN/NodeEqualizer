22915418decdcdeb15c551073891692d3906c13f
Emerson <emersonn956@gmail.com>
2022-02-21 20:22:47 -0300

    Adicionando primerios arquivos de node+equalizer

---
+++ src/database/index.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize')
+const dbConfig = require('../database')
+const User = require('../model/User')
+
+const Connection = new Sequelize(dbConfig)
+
+User.init(Connection)
+
+module.exports = Connection
